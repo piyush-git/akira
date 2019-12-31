458dfb855deac8391ed879e817b992a20b01551d
KarthikeyanRanasthala <karthikeyan.ranasthala@gmail.com>
2019-12-31 12:41:59 +0530

    Added UserDetails Route

---
+++ src/client/src/routes/Dashboard/UserDetails.jsx
@@ -0,0 +1,136 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {
+  Container,
+  Typography,
+  Box,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableRow,
+  Paper
+} from "@material-ui/core";
+
+import styles from "./UserDetails.module.css";
+
+class UserDetails extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    // const {
+    //   renderProps: {
+    //     match: {
+    //       params: { id }
+    //     }
+    //   }
+    // } = this.props;
+    const data = {
+      username: "Karthikeyan",
+      title: "XYZ",
+      reportingTo: "Nikhil",
+      gender: "Male",
+      age: 20,
+      phoneNumber: 123456,
+      address: "1-2-3 ABC",
+      picture:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/User_icon_2.svg/220px-User_icon_2.svg.png"
+    };
+    this.setState({ data });
+  }
+
+  render() {
+    const { data } = this.state;
+    // console.log(data, styles);
+    return (
+      <Container>
+        <Typography variant="h4" component="h2" className={styles.heading}>
+          User Details
+        </Typography>
+        {data ? (
+          <>
+            <Box
+              display="flex"
+              flexDirection="row"
+              p={1}
+              m={1}
+              bgcolor="background.paper"
+              justifyContent="space-around"
+            >
+              <img
+                src={data.picture}
+                alt={data.username}
+                className={styles.picture}
+              />
+            </Box>
+            <TableContainer component={Paper}>
+              <Table>
+                <TableBody>
+                  <TableRow>
+                    <TableCell align="center" component="th" scope="row">
+                      Name:
+                    </TableCell>
+                    <TableCell align="center">{data.username}</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell align="center" component="th" scope="row">
+                      Gender:
+                    </TableCell>
+                    <TableCell align="center">{data.gender}</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell align="center" component="th" scope="row">
+                      Age:
+                    </TableCell>
+                    <TableCell align="center">{data.age}</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell align="center" component="th" scope="row">
+                      Title:
+                    </TableCell>
+                    <TableCell align="center">{data.title}</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell align="center" component="th" scope="row">
+                      Reporting To:
+                    </TableCell>
+                    <TableCell align="center">{data.reportingTo}</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell align="center" component="th" scope="row">
+                      Phone Number:
+                    </TableCell>
+                    <TableCell align="center">{data.phoneNumber}</TableCell>
+                  </TableRow>
+                  <TableRow>
+                    <TableCell align="center" component="th" scope="row">
+                      Address:
+                    </TableCell>
+                    <TableCell align="center">{data.address}</TableCell>
+                  </TableRow>
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </>
+        ) : (
+          <></>
+        )}
+      </Container>
+    );
+  }
+}
+
+UserDetails.propTypes = {
+  renderProps: PropTypes.shape({
+    match: PropTypes.shape({
+      params: PropTypes.shape({
+        id: PropTypes.string.isRequired
+      })
+    })
+  }).isRequired
+};
+
+export default UserDetails;
--- src/client/src/routes/DashboardRoutes.jsx
+++ src/client/src/routes/DashboardRoutes.jsx
@@ -3,12 +3,17 @@ import { Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import ListUsers from "./Dashboard/ListUsers";
+import UserDetails from "./Dashboard/UserDetails";
 
 const DashboardRoutes = props => {
   const { isAuth } = props;
   return isAuth ? (
     <>
       <Route exact path="/dashboard/users" render={() => <ListUsers />} />
+      <Route
+        path="/dashboard/users/:id"
+        render={renderProps => <UserDetails renderProps={renderProps} />}
+      />
     </>
   ) : (
     <Redirect to="/login" />
