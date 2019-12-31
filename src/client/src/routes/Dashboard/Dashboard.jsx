7824a00cb20c1222018f1ea62b6149320ca362f3
Piyush Yadav <work.piyush.yadav@gmail.com>
2019-12-31 15:34:58 +0530

    Added Dashboard route

--- src/client/src/routes/Dashboard/Dashboard.jsx
+++ src/client/src/routes/Dashboard/Dashboard.jsx
@@ -1,7 +1,58 @@
-import React from "react";
+import React, { Component } from "react";
+import { Grid } from "@material-ui/core";
+import styles from "./Dashboard.module.css";
 
-const Dashboard = () => {
-  return <div>DASHBOARD HOME</div>;
-};
+class Dashboard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <Grid container spacing={3}>
+          <Grid item xs={6}>
+            <div className={styles.roundedTopDiv}>
+              <h3 className={styles.headingTop}>Total Assets</h3>
+              <p className={styles.headingBottom}>123</p>
+            </div>
+          </Grid>
+          <Grid item xs={6}>
+            <div className={styles.roundedTopDiv}>
+              <h3 className={styles.headingTop}>Available Assets</h3>
+              <p className={styles.headingBottom}>123</p>
+            </div>
+          </Grid>
+        </Grid>
+
+        <h3>Recent Activities</h3>
+
+        <Grid item xs={12}>
+          <div className={styles.activitiesBox}>
+            <h5 className={styles.dateBox}>12/10/2019</h5>
+            <p className={styles.activityBox}>Action Type - item</p>
+            <p>By Karthikeyan</p>
+          </div>
+        </Grid>
+        <Grid item xs={12}>
+          <div className={styles.activitiesBox}>
+            <h5 className={styles.dateBox}>12/10/2019</h5>
+            <p className={styles.activityBox}>Action Type - item</p>
+            <p>By Piyush</p>
+          </div>
+        </Grid>
+        <Grid item xs={12}>
+          <div className={styles.activitiesBox}>
+            <h5 className={styles.dateBox}>12/10/2019</h5>
+            <p className={styles.activityBox}>Action Type - item</p>
+            <p>By Nikhil</p>
+          </div>
+        </Grid>
+      </div>
+    );
+  }
+}
 
 export default Dashboard;
