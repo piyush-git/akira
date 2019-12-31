1333a35c77910ea1bf6e865712ba321ea3335bb9
KarthikeyanRanasthala <karthikeyan.ranasthala@gmail.com>
2019-12-31 12:15:55 +0530

    Boilerplate Cleanup

--- src/client/src/routes/DashboardRoutes.jsx
+++ src/client/src/routes/DashboardRoutes.jsx
@@ -3,20 +3,12 @@ import { Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 import Dashboard from "./Dashboard/Dashboard";
-import Settings from "./Dashboard/Settings";
-import Profile from "./Dashboard/Profile";
-import Reports from "./Dashboard/Reports";
-import NavBar from "./Dashboard/NavBar";
 
 const DashboardRoutes = props => {
   const { isAuth } = props;
   return isAuth ? (
     <>
-      <Route path="/dash" render={NavBar} />
       <Route path="/dash" exact render={() => <Dashboard />} />
-      <Route path="/dash/settings" render={() => <Settings />} />
-      <Route path="/dash/profile" render={() => <Profile />} />
-      <Route path="/dash/reports" render={() => <Reports />} />
     </>
   ) : (
     <Redirect to="/login" />
