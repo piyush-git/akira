b7e07a78a4e6ace80dfcf6031c0aca0ea95fb227
KarthikeyanRanasthala <karthikeyan.ranasthala@gmail.com>
2020-01-02 20:40:13 +0530

    Assets Log Route

---
+++ src/server/app/main/models/asset_log.py
@@ -0,0 +1,19 @@
+from app.main import db
+from sqlalchemy import Column, Integer, ForeignKey, String, DateTime
+
+
+class Asset_Log(db.Model):
+    """[summary]
+    Arguments:
+        db {[type]} -- [description]
+    """
+    __tablename__ = "assets_log"
+    id = Column(Integer, primary_key=True)
+    item_id = Column(Integer, ForeignKey("assets.id"), nullable=False)
+    assigned_by = Column(
+        Integer, ForeignKey("users.id"), nullable=False)
+    assigned_to = Column(
+        Integer, ForeignKey("users.id"), nullable=False)
+    date_of_assignment = Column(DateTime(timezone=True))
+    date_of_return = Column(DateTime(timezone=True))
+    remarks = Column(String(50))
