7fa641df5d5f41e8f6cec6789c94a5428dc2fa63
023nitu <anilamsingh.nitu023@gmail.com>
2020-01-09 20:14:34 +0530

    GenerateReport get api call

--- src/client/src/routes/Dashboard/GenerateReport.jsx
+++ src/client/src/routes/Dashboard/GenerateReport.jsx
@@ -12,27 +12,39 @@ import axios from "../../utils/axiosInterceptor";
 class GenerateReport extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      assignDate: null,
+      expectedReturnDate: null,
+      reportGenerate: []
+    };
   }
 
-  componentDidMount = () => {
-    axios
-      .get("/")
-      .then(res => {
-        console.log(res);
-      })
-      .catch(error => {
-        console.log(error);
+  handleDateChange = evt => {
+    this.setState({ assignDate: evt });
+  };
+
+  handleReturnDateChange = evt => {
+    this.setState({ expectedReturnDate: evt });
+  };
+
+  handleDateSubmit = Date => {
+    Date.preventDefault();
+    const { assignDate, expectedReturnDate } = this.state;
+    axios.get(` /?${assignDate} & ${expectedReturnDate}`).then(res => {
+      this.setState({
+        reportGenerate: res.data
       });
+    });
   };
 
   render() {
+    const { assignDate, expectedReturnDate, reportGenerate } = this.state;
     return (
       <Container>
         <Typography variant="h4" component="h2" className={styles.heading}>
           Generate Report
         </Typography>
-        <form noValidate>
+        <form noValidate onSubmit={this.handleDateSubmit}>
           <MuiPickersUtilsProvider
             utils={DateFnsUtils}
             className={styles.inputField}
@@ -41,8 +53,9 @@ class GenerateReport extends React.Component {
               margin="normal"
               label="From"
               format="dd/MM/yyyy"
-              // value={assignDate}
-              onChange={this.handleDateChange}
+              // name="assignDate"
+              value={assignDate}
+              onChange={evt => this.handleDateChange(evt)}
               fullWidth
             />
           </MuiPickersUtilsProvider>
@@ -54,12 +67,14 @@ class GenerateReport extends React.Component {
               margin="normal"
               label="To"
               format="dd/MM/yyyy"
-              // value={expectedReturnDate}
-              onChange={this.handleDateChange}
+              value={expectedReturnDate}
+              onChange={evt => this.handleReturnDateChange(evt)}
               fullWidth
             />
           </MuiPickersUtilsProvider>
           <Button
+            href={reportGenerate.download_url}
+            download
             variant="contained"
             color="primary"
             type="submit"
